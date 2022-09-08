ec7fc2f346de3be29ea0201a29fcd4f0d7fb71d8
Sarwar Alam <100376398+srwr-tech@users.noreply.github.com>
2022-09-08 21:30:48 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,3 @@
-
 import React, { Component, useState } from "react";
 import '../styles/App.css';
 
@@ -12,35 +11,42 @@ class App extends Component {
     { name: 'Darjeeling', country: 'India' },
     { name: 'Tokyo', country: 'Japan' },
     { name: 'Lonavala', country: 'India' },
-    { name: 'Brandenburg Gate', country: 'Germany' },
-    { name: 'Reichstag Building', country: 'Germany' },
-    { name: 'Museum Island', country: 'Germany' },
-    { name: 'Munnar', country: 'India' },
-    { name: 'Leh Ladakh', country: 'India' },
-    { name: 'Goa', country: 'India' },
-    { name: 'Agra', country: 'India' },
-    { name: 'Dalhousie', country: 'India' },
-    { name: 'Coorg', country: 'India' },
-    { name: 'Rome', country: 'Italy' },
-    { name: 'Milan', country: 'Italy' },
-    { name: 'Venice', country: 'Italy' },
-    { name: 'Varanasai', country: 'India' },
-    { name: 'Jaipur', country: 'India' },
-    { name: 'The Hofburg', country: 'Austria' },
-    { name: 'Belvedere Palace', country: 'Austria' },
-    { name: 'St. Stephen Cathedral', country: 'Austria' },
-    { name: 'Kahna National Park', country: 'India' },
-    { name: 'Amritsar', country: 'India' },
-    { name: 'Mussoorie', country: 'India' },
-    { name: 'Mount Abu', country: 'India' },
-    { name: 'Tirupati', country: 'India' },
+ 
     ]
+
+    this.finalList = this.sort();
+}
+  getKey(index) {
+      return "location"+(index+1);
+  }
+
+  sort(){
+   
+    const indianCity = []
+     this.cityList.forEach((city) =>{
+        
+        if  (city.country === "India" && !indianCity.includes(city.name))
+        { 
+          console.log(indianCity)
+           indianCity.push(city.name)
+        }
+   })
+      
+    return indianCity;
   }
 
   render() {
+    
+
     return (
       <div id="main">
         {/* Do not remove the main div */}
+        
+        <ol>
+            {this.finalList.map((city,index)=>{
+                return <li key = {this.getKey(index)}>{city} </li>
+              })}
+        </ol>
       </div>
     )
   }
